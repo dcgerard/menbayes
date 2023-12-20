6ff99c1ed263d09f54799bd1278788d1cb5bdc92
David Gerard <gerard.1787@gmail.com>
2023-12-20 17:45:37 -0500

    import from hwep to avoid cran check

--- R/menbayes-package.R
+++ R/menbayes-package.R
@@ -5,6 +5,7 @@
 #' @importFrom rstan sampling
 #' @importFrom RcppParallel CxxFlags
 #' @importFrom rstantools rstan_config
+#' @importFrom hwep simgl
 #' @useDynLib menbayes, .registration = TRUE
 #'
 "_PACKAGE"
